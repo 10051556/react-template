9789fd5ab17c2d7862e58adbc11a90d4603b8cfc
10051556 <43875433+10051556@users.noreply.github.com>
2021-11-18 16:22:55 +0800

    update icon

--- src/Editor.js
+++ src/Editor.js
@@ -10,34 +10,35 @@ import 'ace-builds/src-noconflict/ext-language_tools'
 import 'ace-builds/src-noconflict/ext-beautify'
 
 function App() {
-    const [code, setCode] = useState(`function hello() {
-  console.log("Hello World!");
-}`)
+    const [code, setCode] = useState(`# This program prints Hello, world!
+
+print('Hello, world!')
+`)
 
     return (
         <AceEditor
-            style={{
-                height: '100vh',
-                width: '100%',
-            }}
-            placeholder='Start Coding'
-            mode='javascript'
-            theme='github'
-            name='basic-code-editor'
-            onChange={currentCode => setCode(currentCode)}
-            fontSize={18}
+            placeholder="Placeholder Text"
+            mode="python"
+            theme="github"
+            name="blah2"
+            // onLoad={this.onLoad}
+            // onChange={this.onChange}
+            fontSize={14}
             showPrintMargin={true}
             showGutter={true}
             highlightActiveLine={true}
-            value={code}
+            value={`# This program prints Hello, world!
+
+print('Hello, world!')
+`}
             setOptions={{
                 enableBasicAutocompletion: true,
                 enableLiveAutocompletion: true,
                 enableSnippets: true,
                 showLineNumbers: true,
-                tabSize: 4,
-            }}
-        />
+                tabSize: 2,
+            }} />
+
     )
 }
 
